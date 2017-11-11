fc5e6ab72a3d35049a6dd09dfd9314d72fe18f8f
Andrés Ávila <andaviaco@gmail.com>
2017-11-10 23:28:53 -0600

    define producer's statuses and posible produced chars

--- src/const.js
+++ src/const.js
@@ -1,2 +1,11 @@
 
 export const BUFFER_SIZE = 40;
+
+export const PRODUCER_CHARS = 'ABCDEFGHIJKMLNOPQRSTUVWXYZ0123456789+-*#$%&';
+
+export const PRODUCER_STATUS = {
+  sleeping: 0,
+  working: 1,
+  wakingUp: 2,
+};
+
