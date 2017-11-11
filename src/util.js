01c21b868d4d903e3b32120f4440c3f1ee323c62
Andrés Ávila <andaviaco@gmail.com>
2017-11-10 23:34:42 -0600

    implement util function to set random-timed timeouts

--- src/util.js
+++ src/util.js
@@ -1,7 +1,17 @@
+
 export function getRandomIntInclusive(min, max) {
   const minVal = Math.ceil(min);
   const maxVal = Math.floor(max);
 
   return Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal;
+}
+
+export function setRandomTimeout(fn) {
+  const rtime = getRandomIntInclusive(3, 5) * 1000;
+  console.log('WAIT TIME', rtime);
+
+  const timeoutId = setTimeout(fn, rtime);
+
+  return timeoutId;
 }
 
