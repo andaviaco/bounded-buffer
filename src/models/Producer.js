df9fd6ebb31e6eeb303168efffba645284516894
Andrés Ávila <andaviaco@gmail.com>
2017-11-11 14:30:15 -0600

    validate product conditions

--- src/models/Producer.js
+++ src/models/Producer.js
@@ -2,15 +2,28 @@ import { getRandomIntInclusive } from '../util';
 import { PRODUCER_CHARS, PRODUCER_STATUS } from '../const';
 
 export default class Producer {
-  constructor() {
+  constructor(buffer) {
     this.status = PRODUCER_STATUS.sleeping;
+    this.buffer = buffer;
   }
 
   produce(n=1) {
-    return Array.from(Array(n), () => PRODUCER_CHARS[getRandomIntInclusive(0, PRODUCER_CHARS.length - 1)]);
+    const nitems = n > this.buffer.freeslots? this.buffer.freeslots : n;
+    if (!this.buffer.isAvailable && this.buffer.freeslots) {
+      console.log('try produce: NOPE');
+      return null;
+    }
+
+    const product = Array.from(Array(nitems), () => PRODUCER_CHARS[getRandomIntInclusive(0, PRODUCER_CHARS.length - 1)]);
+
+    return this.buffer.insert(product);
   }
 
   randProduct() {
+    if (!this.buffer.isAvailable && this.buffer.freeslots) {
+      return null;
+    }
+    
     const product = this.produce(getRandomIntInclusive(2, 8));
 
     return product;
