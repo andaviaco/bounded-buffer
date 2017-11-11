74f3f78d060304db0b9cbcc269e214ae8a17f4c7
Andrés Ávila <andaviaco@gmail.com>
2017-11-11 14:32:14 -0600

    remove '-' from produceable chars

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 
 export const BUFFER_SIZE = 40;
 
-export const PRODUCER_CHARS = 'ABCDEFGHIJKMLNOPQRSTUVWXYZ0123456789+-*#$%&';
+export const PRODUCER_CHARS = 'ABCDEFGHIJKMLNOPQRSTUVWXYZ0123456789+*#$%&';
 
 export const PRODUCER_STATUS = {
   sleeping: 0,
