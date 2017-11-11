4aa06ee58f572d4fa70e333527925a1eb413e930
Andrés Ávila <andaviaco@gmail.com>
2017-11-10 23:30:36 -0600

    implement "produce" method

--- src/models/Producer.js
+++ src/models/Producer.js
@@ -1,7 +1,13 @@
+import { getRandomIntInclusive } from '../util';
+import { PRODUCER_CHARS, PRODUCER_STATUS } from '../const';
 
 export default class Producer {
   constructor() {
+    this.status = PRODUCER_STATUS.sleeping;
+  }
 
+  produce(n=1) {
+    return Array.from(Array(n), () => PRODUCER_CHARS[getRandomIntInclusive(0, PRODUCER_CHARS.length - 1)]);
   }
 
 
